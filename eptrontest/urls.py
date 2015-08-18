82a584519c9bfc58173eeaac062e7a82b51b09d7
Arturs Priede <arturs.priede88@gmail.com>
2015-08-18 16:11:48 +0100

    Admin page

--- eptrontest/urls.py
+++ eptrontest/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
+
 from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', include(admin.site.urls)),
 ]
